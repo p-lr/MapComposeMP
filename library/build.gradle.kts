20d8d91b6e3308d9d339d6ec8249ef1fc570eb79
stashymane <aestheticrice@gmail.com>
2024-11-21 18:23:31 +0200

    fix jvm versoin mismatch

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -20,7 +20,11 @@ kotlin {
         publishLibraryVariants("release")
     }
     
-    jvm("desktop")
+    jvm("desktop") {
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_11)
+        }
+    }
 
     listOf(
         iosX64(),
@@ -86,8 +90,8 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
     dependencies {
         debugImplementation(libs.compose.ui.tooling)
