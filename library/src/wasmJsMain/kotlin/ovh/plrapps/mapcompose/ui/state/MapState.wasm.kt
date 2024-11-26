9433759a10712758b1075a4dfc693eb5df3fff47
stashymane <aestheticrice@gmail.com>
2024-11-26 11:32:23 +0200

    Add clarification for getProcessorCount in wasmJs

--- library/src/wasmJsMain/kotlin/ovh/plrapps/mapcompose/ui/state/MapState.wasm.kt
+++ library/src/wasmJsMain/kotlin/ovh/plrapps/mapcompose/ui/state/MapState.wasm.kt
@@ -1,5 +1,8 @@
 package ovh.plrapps.mapcompose.ui.state
 
+/* This function is somewhat misleading in wasmJs - this dictates how many coroutines are used for fetching map tiles,
+     however, fetching on the web is done asynchronously already, so this is limited less by thread/processor count and
+     more by how many concurrent connections the browser supports, or a map tile platform allows. */
 actual fun getProcessorCount(): Int {
     return 8
 }
