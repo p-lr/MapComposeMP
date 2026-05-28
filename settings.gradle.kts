579fbe5626d1f1834fed5de6670acf8a3bfb4acf
plr <plr.devs@gmail.com>
2026-05-28 18:58:08 +0200

    Remove unnecessary repo

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,7 +3,6 @@ enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
 pluginManagement {
     repositories {
-        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
         google()
         gradlePluginPortal()
         mavenCentral()
@@ -17,7 +16,6 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
-        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
     }
 }
 
